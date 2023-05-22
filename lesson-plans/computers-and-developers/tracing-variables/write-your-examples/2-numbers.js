327821ca03487e93981fa5d2a9e4c3fc0b97fdab
samirm00 <sammoham.be@gmail.com>
2023-05-22 06:17:39 +0200

    update tracing variables

---
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/2-numbers.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* Numbers
+
+TODO: create a few variables(numbers):
+- you need to add comments to show where the variable been
+declared, initialized, read , reassigned,...
+*/
---
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/4-null.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* Null
+
+TODO: create a few variables(null):
+- you need to add comments to show where the variable been
+declared, initialized, read , reassigned,...
+*/
