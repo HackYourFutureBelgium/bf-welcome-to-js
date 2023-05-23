43103737716d8035746760236a8a27606d9f996e
Rekhasriraman <rekha.sri85@gmail.com>
2023-05-23 23:41:49 +0200

    add trace-variable1

--- lesson-plans/computers-and-developers/tracing-variables/exercises/3-hello-good-bye.js
+++ lesson-plans/computers-and-developers/tracing-variables/exercises/3-hello-good-bye.js
@@ -5,16 +5,21 @@
   Add comments and logs to help a developer understand this program
 
 */
-
+// declare a variable first and initialize it to the value 'Pieter' 
 let first = 'Pieter';
+// declare a variable last and initialize it to the value 'Rabbit'
 let last = 'Rabbit';
-
+// read the variable first
+// read the variable last
+// declare a variable fullName and initialize it to the value 'first + ' ' + last'
 let fullName = first + ' ' + last;
-
+// read the variable fullName
 console.log(fullName);
-
+// read the variable fullName
+// declare a variable greeting and initialize it to the value 'hello ' + fullName
 let greeting = 'hello ' + fullName;
-
+// read the variable fullName
+// reassign the variable greeting and initialize it to the value 'good bye ' + fullName
 greeting = 'good bye ' + fullName;
-
+// read the variable greeting
 console.log(greeting);
--- lesson-plans/computers-and-developers/tracing-variables/exercises/4-value-swap.js
+++ lesson-plans/computers-and-developers/tracing-variables/exercises/4-value-swap.js
@@ -8,17 +8,28 @@
   study tips: [variables], [open in] jsTutor, [ask me]
 
 */
-
+// declare a variable a and initialize it to the value 'y' 
 let a = 'y';
+// declare a variable b and initialize it to the value 'x' 
 let b = 'x';
+// declare a variable t and initialize it to the value ' ' 
 let t = '';
-
+// read the variable a
+// read the variable b
+// read the variable t
 console.log(a, b, t);
-
+// read the variable a
+// reassign the variable t and initialize it to the value a
 t = a;
+// read the variable b
+// reassign the variable a and initialize it to the value b
 a = b;
+// read the variable t
+// reassign the variable b and initialize it to the value t
 b = t;
-
+// read the value a
+// read the value b
+// read the value t
 console.log(a, b, t);
 
 // what did you do in this program?
