6b81fea607016d39f306a8971c87babac609a6d5
iradukundajp <ikundajp@gmail.com>
2023-05-23 21:42:56 +0200

    add trace variables exercises

--- lesson-plans/computers-and-developers/tracing-variables/exercises/1-two-variables.js
+++ lesson-plans/computers-and-developers/tracing-variables/exercises/1-two-variables.js
@@ -10,15 +10,15 @@
 */
 
 // trace the program a few times with different values
-
+// declare a variable x and initialize it to the value 'Pieter'
 let x = 'Pieter';
 // let x = 'Blake';
 // let x = 'George';
-
+// declare a variable y and initialize it to the value 'hello' + 'Pieter'
 let y = 'hello ' + x;
-
+// Read the valiable y
 console.log(y);
-
+// reassign  a variable y and initialize it to the value 'good bye ' + 'Pieter'
 y = 'good bye ' + x;
-
+// Read the valiable y
 console.log(y);
--- lesson-plans/computers-and-developers/tracing-variables/exercises/2-four-variables.js
+++ lesson-plans/computers-and-developers/tracing-variables/exercises/2-four-variables.js
@@ -10,21 +10,21 @@
 */
 
 // trace the program a few times with different values
-
+// declare a variable a and initialize it to the value 'Pieter'
 let a = 'Pieter';
 // let a = 'Blake';
 // let a = 'George';
-
+// declare a variable b and initialize it to the value 'Rabbit'
 let b = 'Rabbit';
 // let b = 'Wozzeck';
 // let b = 'Schoenberg';
-
+// read a variable a and b, declare variable c and initialize it to value 'Peter' + ' ' + 'Rabbit'
 let c = a + ' ' + b;
-
+// read a variable c, declare variable d and initialize it to value 'Hello' + 'Peter' + ' ' + 'Rabbit'
 let d = 'hello ' + c;
-
+// read the variable d
 console.log(d);
-
+//// read a variable c, assign variable d and initialize it to value 'good bye' + 'Peter' + ' ' + 'Rabbit
 d = 'good bye ' + c;
-
+// read the variable d
 console.log(d);
