f25d79dc25f8edaccbb029361498525ec68e66f4
rahwakinfe <your email>
2023-05-24 09:04:48 +0200

    add variables

--- lesson-plans/computers-and-developers/tracing-variables/exercises/1-two-variables.js
+++ lesson-plans/computers-and-developers/tracing-variables/exercises/1-two-variables.js
@@ -10,15 +10,17 @@
 */
 
 // trace the program a few times with different values
-
+// declare variable x and initialize it to the value 'Pieter'
 let x = 'Pieter';
 // let x = 'Blake';
 // let x = 'George';
-
+// read a variable x 
+// declare variable y and initialize it to the value 'hello' + 'Pieter';
 let y = 'hello ' + x;
-
+// read variable y
 console.log(y);
-
+// read a variable x
+// assign variable y and initialize it to the value 'good bye' + 'Pieter'
 y = 'good bye ' + x;
-
+// read variable y
 console.log(y);
--- lesson-plans/computers-and-developers/tracing-variables/exercises/4-value-swap.js
+++ lesson-plans/computers-and-developers/tracing-variables/exercises/4-value-swap.js
@@ -8,17 +8,32 @@
   study tips: [variables], [open in] jsTutor, [ask me]
 
 */
-
+// declare a variable a and initialize it to a value 'y'
 let a = 'y';
+// declare a variable b and initialize it to a value 'x'
 let b = 'x';
+// declare a variable t and initialize it to a value ' '
 let t = '';
 
+// read a variable a with value 'y'
+// read a variable b with value 'x'
+// read a variable t with value ' '
 console.log(a, b, t);
-
+// read the variable a with the value 'y'
+// assign the variable t to the value 'y'
 t = a;
+
+// read the variable b with the value 'x'
+// assign the variable a to the value 'x'
 a = b;
+
+// read the variable t with the value 'y'
+// assign the variable b to the value  'y'
 b = t;
 
+// read the variable a with the value 'x'
+// read the variable b with the value 'y'
+// read the variable t with the value 'y'
 console.log(a, b, t);
 
 // what did you do in this program?
