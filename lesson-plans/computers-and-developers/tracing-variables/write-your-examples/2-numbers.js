a7b4c1f9badcfe2b1e35875bfbd836917e88abed
rahwakinfe <your email>
2023-05-24 15:37:01 +0200

    add changes

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/2-numbers.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/2-numbers.js
@@ -6,27 +6,25 @@ TODO: create a few variables(numbers):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
-'use strict';
-
 
 // declared
 let sum;
 
 // declared the variable a & initialized to value 20
-let a = 20; 
+let a = 20;
 
 // declared & initialized
 let b = 30;
 
-// read the variable a, 
+// read the variable a,
 // read the variable b, initialized
- sum = a + b;
+sum = a + b;
 
 // read a, read b, read sum
 console.log(`sum: ${a} + ${b}  = ${sum}`);
 
 // read sum, read b, reassigned a
-a = sum + b; 
+a = sum + b;
 
 // read sum, read b, read a
-console.log(`new a : ${sum} + ${b} = ${a}`); 
+console.log(`new a : ${sum} + ${b} = ${a}`);
--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/5-undefined.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/5-undefined.js
@@ -2,7 +2,23 @@
 
 /* Undefined
 
-TODO: create a few variables(undefined):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
+// declare a variable 'undefinedVariable' initializing it to value 'undefined
+let undefinedVariable;
+
+// read the value of the variable 'undefinedVariable' (which is currently undefined)
+console.log(undefinedVariable);
+
+// assign a value of undefined to the variable 'undefinedVariable'
+undefinedVariable = undefined;
+
+// read the updated value of the variable 'undefinedVariable'
+console.log(undefinedVariable);
+
+// declare a variable 'anotherUndefined' and initialize it with the value undefined
+let anotherUndefined;
+
+// read the value of the variable 'anotherUndefined' (which is currently undefined)
+console.log(anotherUndefined);
