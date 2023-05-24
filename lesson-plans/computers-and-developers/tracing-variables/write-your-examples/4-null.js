98c6e92dbc1b4623a36de2269bd2a569c1eb52f7
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-05-24 10:07:16 +0200

    trace-variables added

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/4-null.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/4-null.js
@@ -6,3 +6,18 @@ TODO: create a few variables(null):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
+
+/* declaring variables and initializing them to null */
+let month = null;
+let year = null;
+
+/* reading the variables type */
+console.log(typeof month);
+console.log(typeof year);
+
+/* reassigning values to the variables */
+month = 'march';
+year = 1995;
+
+/* reading the values of the variables */
+console.log('You were born in ' + month + ', ' + year);
--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/5-undefined.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/5-undefined.js
@@ -6,3 +6,15 @@ TODO: create a few variables(undefined):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
+
+/* declaring a varibale and initializing it to undefined  */
+let favoriteAnimal = undefined;
+
+/* reading the value of the variable */
+console.log(typeof favoriteAnimal);
+
+/* reassigning the values of the variable */
+favoriteAnimal = 'koala';
+
+/* reading the value of the variable */
+console.log('Her favorite animal is the ' + favoriteAnimal);
