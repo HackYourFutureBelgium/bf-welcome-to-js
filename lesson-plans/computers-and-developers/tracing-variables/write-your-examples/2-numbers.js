089f373a3b47679269524275443791a337b7e297
Rekhasriraman <rekha.sri85@gmail.com>
2023-05-24 11:10:33 +0200

    add examples

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/2-numbers.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/2-numbers.js
@@ -6,3 +6,16 @@ TODO: create a few variables(numbers):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
+
+// declare a variable number1 and initialize it to the value 50
+let number1 = 50;
+// declare a variable number2 and initialize it to the value 20
+let number2 = 20;
+// read the variable number1
+// read the variable number2
+// declare a variable sum and initialize it to the value number1 + number2
+let sum = number1 + number2
+//read the variable sum
+console.log(sum)
+
+
