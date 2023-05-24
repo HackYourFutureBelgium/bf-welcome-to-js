f9818261a78c7e98173d7e5d21404286d9ad264e
iradukundajp <ikundajp@gmail.com>
2023-05-24 12:31:09 +0200

    add trace variables examples

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/1-strings.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/1-strings.js
@@ -6,3 +6,20 @@ TODO: create a few variables(strings):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
+
+// declare a variable name and initialise to it value 'John Paul '
+let name = 'John Paul '; 
+ 
+// declare a variable city and initialise to it value 'Antwerp'
+let city = 'Antwerp';  
+
+// declare a variable name and initialise to it value name name + 'from ' + city 
+let adress = name + 'from ' + city ; 
+// read variable adress
+console.log(adress); 
+
+// declare variable message  and initialize it
+let message = `
+Hello my name is ${name} and I am from ${city}`;
+// read variable message
+console.log(message); 
