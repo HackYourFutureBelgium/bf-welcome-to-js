d92e2c61292df70085988cd49e4a15c9da295d0e
komal-89 <kp30512@gmail.com>
2023-05-24 12:02:27 +0200

    varaible

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/1-strings.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/1-strings.js
@@ -6,3 +6,16 @@ TODO: create a few variables(strings):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
+// 'variable  'name' declaration and initialization
+let name = 'Tushar';
+
+// variable 'fullname' declaration and initialization.
+let fullname = 'Tushar dev';
+
+// Reassingning the value of variable 'name'
+name = 'Anna';
+
+// printing the values of variables 'name' and 'fullname'
+console.log(name, fullname);
+
+// read 'name' and 'fullname'
