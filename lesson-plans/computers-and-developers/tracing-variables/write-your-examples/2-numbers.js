c011dcbd6a23a85176327d126829ffa95d8efc4f
iradukundajp <ikundajp@gmail.com>
2023-05-24 12:44:36 +0200

    add trace variables exercises  examples

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/2-numbers.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/2-numbers.js
@@ -6,18 +6,18 @@ TODO: create a few variables(numbers):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
-// declare variable birthYear and initialize it to the value 1998
+// declare variable birthYear and initialize it to the value 1998.
 let birthYear = 1998 ;
 
-// declare variable birthYear and initialize it to the value 2023
+// declare variable birthYear and initialize it to the value 2023.
 let currentYear = 2023 ;
 
-// declare variable birthYear and initialize it to the value currentYear - birthYear
+// declare variable birthYear and initialize it to the value currentYear - birthYear.
 let age = currentYear - birthYear ;
 
-// declare variable message  and initialize it 
+// declare variable message  and initialize it.
 let message = `
 You are ${age} years old.`;
 
-// read variable age
+// read variable age.
 console.log(message) ;
