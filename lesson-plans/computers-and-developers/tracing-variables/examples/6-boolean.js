daa76978387df4dbb12e3890f6444a7b0596a2ff
IrynaSobetska <sobetska.ira@gmail.com>
2023-05-25 06:45:50 +0200

    Fix trace variables

--- lesson-plans/computers-and-developers/tracing-variables/examples/6-boolean.js
+++ lesson-plans/computers-and-developers/tracing-variables/examples/6-boolean.js
@@ -9,29 +9,10 @@
   3- booleans are very useful in comparisons and conditions
 */
 
-// declare a variable age and initialize it to the value '15'
-let age = 15;
+let isOldEnough = false;
 
-//read the variable age with value '15'
-//check the conditions, else if (age < 18) is true
-//read the message 'You are a teenager'
-// assign the variable age to the value 'You are a teenager'
-if (age < 18) {
-  console.log('You are a teenager');
-  age = 'You are a teenager'
+if (isOldEnough) {
+  console.log('You can drive!');
 } else {
-  console.log('You are an adult');
-  age = 'You are an adult'
-}
-
-// declare a variable age2 and initialize it to the value '20'
-let age2 = 20;
-
-//read the variable age2 with value '20'
-//check the conditions, else if (age < 18) is false
-//read the message 'You are an adult'
-if (age2 < 18) {
-  console.log('You are a teenager');
-} else {
-  console.log('You are an adult');
+  console.log("Sorry you can't drive, you are not old enough");
 }
--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/3-booleans.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/3-booleans.js
@@ -8,36 +8,12 @@ declared, initialized, read , reassigned,...
 */
 
 // declare a variable age and initialize it to the value 15
-let age = 15;
+let age = false;
 
-//read the variable age with value 15
-//check the conditions, else if (age < 20) is true
-//read the variable age with value 'You are a teenager'
-if (age < 20) {
+//check the conditions, is false
+//read the variable age with value 'You are an adult'
+if  (age){
   console.log('You are a teenager');
 } else {
   console.log('You are an adult');
 }
-
-// declare a variable number and initialize it to the value 2
-let number = 2;
-
-
-//read the variable number with value 2
-//check the condition while (number < 2 * 3), true
-//read the variable number with value 2
-//assign the variable number to the value 4
-
-//read the variable number with value 4
-//check the condition while (number < 2 * 3), true
-//read the variable number with value 4
-//assign the variable number to the value 6
-
-//read the variable number with value 6
-//check the condition while (number < 2 * 3), false
-while (number < 2 * 3) {
-  number = number + 2;
-}
-
-//read the variable number with value 6
-console.log(number)
