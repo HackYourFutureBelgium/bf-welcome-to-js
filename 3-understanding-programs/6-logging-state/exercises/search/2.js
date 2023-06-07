a923ba51cd3b2e121da57bfa260f101a1fd40414
Preethi Tapati <118854537+PreethiTapati@users.noreply.github.com>
2023-06-07 15:47:43 +0200

    Update 2.js

--- 3-understanding-programs/6-logging-state/exercises/search/2.js
+++ 3-understanding-programs/6-logging-state/exercises/search/2.js
@@ -1,70 +1,3 @@
-// #todo
-
- 'use strict';
-
-// /*
-//   log the user's phrase
-//   log which type of search they wanted
-//   log their search query
-//   log whether or not the query was found in their phrase
-// */
-
-// let phrase = '';
-
-// let userConfirmedPhrase = false;
-// while (!userConfirmedPhrase) {
-//   phrase = prompt('enter a phrase to search');
-//   if (phrase === null) {
-//     continue;
-//   }
-
-//   userConfirmedPhrase = confirm('is this correct: "' + phrase + '"');
-// }
-
-// const caseSensitive = confirm('do you want a case-sensitive search?');
-
-// let query = '';
-
-// let userConfirmedQuery = false;
-// while (!userConfirmedQuery) {
-//   query = prompt(
-//     'enter a search query, this program will check if it exists in your phrase',
-//   );
-//   if (query === null) {
-//     continue;
-//   }
-
-//   userConfirmedQuery = confirm('is this correct: "' + query + '"');
-// }
-
-// let phraseIncludesQuery;
-// if (caseSensitive) {
-//   phraseIncludesQuery = phrase.includes(query);
-// } else {
-//   const lowerCasePhrase = phrase.toLowerCase();
-//   const lowerCaseQuery = query.toLowerCase();
-//   phraseIncludesQuery = lowerCasePhrase.includes(lowerCaseQuery);
-// }
-
-// let doesOrNot = '';
-// if (phraseIncludesQuery) {
-//   doesOrNot = 'does';
-// } else {
-//   doesOrNot = 'does not';
-// }
-
-// alert(
-//   '"' +
-//     phrase +
-//     '" ' +
-//     doesOrNot +
-//     ' include "' +
-//     query +
-//     '"\n\n' +
-//     'case sensitive: ' +
-//     caseSensitive,
-// );
-
 'use strict';
 
 /*
@@ -86,11 +19,11 @@ while (!userConfirmedPhrase) {
   userConfirmedPhrase = confirm('Is this correct: "' + phrase + '"');
 }
 
-console.log('User entered phrase:', phrase);
+console.log( phrase); // log the user's phrase
 
 const caseSensitive = confirm('Do you want a case-sensitive search?');
 
-console.log('Case-sensitive search:', caseSensitive);
+console.log('Case-sensitive search:', caseSensitive); //log which type of search they wanted
 
 let query = '';
 
@@ -100,11 +33,9 @@ while (!userConfirmedQuery) {
   if (query === null) {
     continue;
   }
-
-  userConfirmedQuery = confirm('Is this correct: "' + query + '"');
+ userConfirmedQuery = confirm('Is this correct: "' + query + '"');
 }
-
-console.log('User entered search query:', query);
+console.log(query);  //log their search query
 
 let phraseIncludesQuery;
 if (caseSensitive) {
@@ -115,7 +46,7 @@ if (caseSensitive) {
   phraseIncludesQuery = lowerCasePhrase.includes(lowerCaseQuery);
 }
 
-console.log('Query found in the phrase:', phraseIncludesQuery);
+console.log(phraseIncludesQuery);   // log whether or not the query was found in their phrase
 
 let doesOrNot = phraseIncludesQuery ? 'does' : 'does not';
 
