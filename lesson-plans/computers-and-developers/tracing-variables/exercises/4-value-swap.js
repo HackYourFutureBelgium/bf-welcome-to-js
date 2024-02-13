81ca03e8ffce4071a26dcf3d477b36fc90f8703b
ahlamboudali <hanane.boudali@gmail.com>
2024-02-13 13:55:24 +0100

    solve-value-swap

--- lesson-plans/computers-and-developers/tracing-variables/exercises/4-value-swap.js
+++ lesson-plans/computers-and-developers/tracing-variables/exercises/4-value-swap.js
@@ -8,21 +8,31 @@
   study tips: [variables], [open in] jsTutor, [ask me]
 
 */
-
+// Declare and initialize variables
 let a = 'y';
 let b = 'x';
 let t = '';
 
+// Log initial values of a, b, and t
 console.log(a, b, t);
 
+// Swap the values of a and b using a temporary variable t
 t = a;
 a = b;
 b = t;
 
+// Log updated values of a, b, and t after the swap
 console.log(a, b, t);
 
 // what did you do in this program?
+// Initially, a is 'y' and b is 'x'.
+
 // what are the final values of `a` and `b`?
+// After the swap, a becomes 'x' and b becomes 'y'.
+
 // how are they changed from the initial values
+// The temporary variable t is used to hold the value of a during the swap operation.
 
 // is it possible to swap a and b without using t?
+// Yes, it's possible to swap a and b without using a temporary variable
+// but it involves using arithmetic or bitwise operations, which may be less readable and more complex.
