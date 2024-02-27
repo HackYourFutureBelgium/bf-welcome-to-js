e98c45838dbc0aafd3396f6b3af777b63dce99e9
dspodina <spodina.darya@gmail.com>
2024-02-27 12:33:48 +0700

    added 2-reverse task

--- lesson-plans/devtools-debugger/1-isolate/exercises/3-for-of-loops/2-reverse.js
+++ lesson-plans/devtools-debugger/1-isolate/exercises/3-for-of-loops/2-reverse.js
@@ -17,3 +17,20 @@ alert(transformation);
 /*  ===== Challenges =====
   - Put the characters back in the right order after the string was reversed.
 */
+
+// Solution
+
+let toReverse = "Hello world! You're awesome!";
+let backwards = '';
+for (let nextChar of toReverse) {
+  backwards = nextChar + backwards;
+}
+
+let forwards = '';
+for (let nextChar of backwards) {
+  forwards = nextChar + forwards;
+}
+
+let transformation = toReverse + ' -> ' + backwards;
+
+console.log(transformation);
